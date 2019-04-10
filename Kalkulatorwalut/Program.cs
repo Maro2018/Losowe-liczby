2f040ea080f23dff156f9c8990233473282acfab
Maro2018 <mdudzik@protonmail.com>
2019-04-10 13:59:26 +0200

    pierwsze

---
+++ Kalkulatorwalut/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulatorwalut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int poczatek = 1;
+            int koniec = 48;
+
+            int[] liczby = new int [6];
+            
+            for (int i=0; i < liczby.Length; i++)
+            {
+                int losowana = rnd.Next(poczatek, koniec);
+                liczby[i] = losowana;
+                Console.WriteLine(liczby[i]);
+            }
+
+            
+            
+
+            
+
+            //int waga = 5;
+            //string imie = "kotel";
+            //Console.Write("Kot nazywa sie: {0} waga kota to: {1}",waga,imie);
+            Console.ReadKey();
+        }
+    }
+}
